769cc1f86ef0a6cdef7362638991a6a0bb5d6647
humma-irshad <76107314+humma-irshad@users.noreply.github.com>
2022-02-02 21:59:37 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -23,11 +23,11 @@ class QuizInterface():
         self.canvas.config(width='300', height='250', bg='white')
         self.canvas.grid(row='1', column='0', columnspan='2', pady='50')
 
-        true_img = PhotoImage(file='images/true.png')
+        true_img = PhotoImage(file='true.png')
         self.true_button = Button(image=true_img, command=self.true_pressed)
         self.true_button.grid(row='2', column='0')
 
-        false_img = PhotoImage(file='images/false.png')
+        false_img = PhotoImage(file='false.png')
         self.false_button = Button(image=false_img, command=self.false_pressed)
         self.false_button.grid(row='2', column='1')
 
